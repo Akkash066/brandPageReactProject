c5df9ad2f5ba8d63c4c88662877a66b568b3ee3a
Akash <akashk494233@gmail.com>
2025-02-11 16:16:38 +0530

    first react project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css"
+import Navigation from "./components/Navigation.jsx";
+import MainSection from "./components/MainSection.jsx";
+
+const App = () =>{
+
+
+  return(
+  <>
+  <Navigation />
+  <MainSection />
+  </>
+  );
+};
+
+export default App;
